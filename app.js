891c01ec18f311f3dbc9517d415b3273333d3df9
yousef-waleed <dev.yousef.waleed@gmail.com>
2023-07-27 00:04:57 +0300

    main style and  learning fetch

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 let url = "https://api.adviceslip.com/advice";
+
+
+async function quote() {
+    const response = await fetch(url);
+    const quotes = response.json().then(text=>{
+        return text
+    });
+}
+
+let q = quote();
+
+
+
